544a3b81a4b862a7b09b9c747e61a69384ad3e25
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-02-01 08:18:25 +0900

    💄 認証関連の画面のUI作成

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from 'react'
+
+const AuthLayout = ({ children }: { children: ReactNode }) => {
+	return (
+		<div className="h-full flex items-center justify-center bg-[#5C3B58]">
+			<div className="md:h-auto md:w-[420px]">{children}</div>
+		</div>
+	)
+}
+
+export default AuthLayout
