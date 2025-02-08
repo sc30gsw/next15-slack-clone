1c3be8144f894986eddff95ff92df96b698b1e2e
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-02-08 12:28:33 +0900

    ✨ 認証処理のUI実装

--- src/features/auth/components/sign-up-form.tsx
+++ src/features/auth/components/sign-up-form.tsx
@@ -1,60 +1,109 @@
 'use client'
 
-import { Button, Form, Separator, TextField } from '@/components/justd/ui'
+import {
+	Button,
+	Form,
+	Loader,
+	Separator,
+	TextField,
+} from '@/components/justd/ui'
 import { OauthButton } from '@/features/auth/components/oauth-button'
-import { IconBrandGithub } from 'justd-icons'
+import { useOauthSignIn } from '@/features/auth/hooks/use-oauth-sign-in'
+import { useSignUp } from '@/features/auth/hooks/use-sign-up'
+import { getFormProps, getInputProps } from '@conform-to/react'
+import { IconBrandGithub, IconBrandGoogle } from 'justd-icons'
+import { Fragment } from 'react'
 
 export const SignUpForm = () => {
+	const { form, action, lastResult, isPending, fields } = useSignUp()
+	const { isPending: isOauthPending, action: oauthAction } = useOauthSignIn()
+
 	return (
 		<>
-			<Form className="space-y-2.5">
-				<TextField
-					type="email"
-					placeholder="Email"
-					value=""
-					onChange={() => {}}
-					isDisabled={false}
-					isRequired={true}
-				/>
-				<TextField
-					type="password"
-					placeholder="Password"
-					value=""
-					onChange={() => {}}
-					isDisabled={false}
-					isRequired={true}
-				/>
-				<TextField
-					type="password"
-					placeholder="Confirm Password"
-					value=""
-					onChange={() => {}}
-					isDisabled={false}
-					isRequired={true}
-				/>
+			<Form {...getFormProps(form)} action={action} className="space-y-2.5">
+				<div>
+					<TextField
+						{...getInputProps(fields.email, { type: 'email' })}
+						placeholder="Email"
+						defaultValue={lastResult?.initialValue?.email.toString() ?? ''}
+						isDisabled={isPending || isOauthPending}
+						errorMessage={''}
+					/>
+					<span id={fields.email.errorId} className="text-sm text-red-500">
+						{fields.email.errors}
+					</span>
+				</div>
+				<div>
+					<TextField
+						{...getInputProps(fields.password, { type: 'password' })}
+						placeholder="Password"
+						defaultValue={lastResult?.initialValue?.password.toString() ?? ''}
+						isDisabled={isPending || isOauthPending}
+						errorMessage={''}
+					/>
+					<span id={fields.password.errorId} className="text-sm text-red-500">
+						{fields.password.errors && fields.password.errors?.length > 1
+							? fields.password.errors.map((error) => (
+									<Fragment key={error}>
+										{error}
+										<br />
+									</Fragment>
+								))
+							: fields.password.errors}
+					</span>
+				</div>
+				<div>
+					<TextField
+						{...getInputProps(fields.passwordConfirmation, {
+							type: 'password',
+						})}
+						placeholder="Password Confirmation"
+						defaultValue={
+							lastResult?.initialValue?.passwordConfirmation.toString() ?? ''
+						}
+						isDisabled={isPending || isOauthPending}
+						errorMessage={''}
+					/>
+					<span
+						id={fields.passwordConfirmation.errorId}
+						className="text-sm text-red-500"
+					>
+						{fields.passwordConfirmation.errors &&
+						fields.passwordConfirmation.errors?.length > 1
+							? fields.passwordConfirmation.errors.map((error) => (
+									<Fragment key={error}>
+										{error}
+										<br />
+									</Fragment>
+								))
+							: fields.passwordConfirmation.errors}
+					</span>
+				</div>
 				<Button
 					type="submit"
 					intent="secondary"
-					// appearance="outline"
 					size="large"
-					isDisabled={false}
-					className="w-full text-white bg-zinc-900 hover:bg-zinc-950 data-hovered:bg-zinc-800/90 data-pressed:bg-zinc-800/90"
+					isDisabled={isPending || isOauthPending}
+					className="w-full text-white bg-zinc-900 hover:bg-zinc-950 data-hovered:bg-zinc-800/90 data-pressed:bg-zinc-800/90 relative"
 				>
 					Continue
+					{isPending && <Loader className="absolute top-3 right-3" />}
 				</Button>
 			</Form>
 			<Separator />
 			<div className="flex flex-col gap-y-2.5">
 				<OauthButton
-					isDisabled={false}
-					onClick={() => {}}
+					isDisabled={isOauthPending}
+					isPending={isPending}
+					onClick={() => oauthAction('github')}
 					icon={IconBrandGithub}
 					label="Continue with Github"
 				/>
 				<OauthButton
-					isDisabled={false}
-					onClick={() => {}}
-					icon={IconBrandGithub}
+					isDisabled={isOauthPending}
+					isPending={isPending}
+					onClick={() => oauthAction('google')}
+					icon={IconBrandGoogle}
 					label="Continue with Github"
 				/>
 			</div>
