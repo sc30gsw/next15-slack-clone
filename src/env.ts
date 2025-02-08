af928c7a7eade3affb8651c9e87b3144e9d76293
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-02-08 12:28:57 +0900

    🏷️ 環境変数追加

--- src/env.ts
+++ src/env.ts
@@ -3,8 +3,13 @@ import { z } from 'zod'
 
 export const env = createEnv({
 	server: {
-		TURSO_CONNECTION_URL: z.string().url(),
 		TURSO_AUTH_TOKEN: z.string(),
+		TURSO_CONNECTION_URL: z.string().url(),
+		AUTH_SECRET: z.string(),
+		GITHUB_CLIENT_ID: z.string(),
+		GITHUB_CLIENT_SECRET: z.string(),
+		GOOGLE_CLIENT_ID: z.string(),
+		GOOGLE_CLIENT_SECRET: z.string(),
 	},
 	client: {
 		NEXT_PUBLIC_APP_URL: z.string().url(),
@@ -12,6 +17,11 @@ export const env = createEnv({
 	runtimeEnv: {
 		TURSO_CONNECTION_URL: process.env.TURSO_CONNECTION_URL,
 		TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
+		AUTH_SECRET: process.env.AUTH_SECRET,
+		GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+		GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+		GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+		GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 		NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
 	},
 })
