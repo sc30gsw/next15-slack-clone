dba35b7a53019c25d99ca459c2ebf19726229d5a
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-02-15 17:57:30 +0900

    ➕ server-onlyを追加

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,7 +1,24 @@
+import { notFound, redirect } from 'next/navigation'
+import 'server-only'
+
 type FetchArgs = Parameters<typeof fetch>
 
 export const fetcher = async <T>(url: FetchArgs[0], args?: FetchArgs[1]) => {
   const res = await fetch(url, args)
+
+  switch (res.status) {
+    case 404:
+      notFound()
+      break
+
+    case 401:
+      redirect('/sign-in')
+      break
+
+    default:
+      break
+  }
+
   const json: T = await res.json()
 
   return json
