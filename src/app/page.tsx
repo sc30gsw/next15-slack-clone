4220690600ea6c3062435dce4e3280b2b47655b4
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-02-15 17:58:37 +0900

    🔥 不要fetchの削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 import { UserButton } from '@/features/auth/components/user-button'
-
-// const getTasks = async () => {
-//   const url = client.api.tasks.$url()
-//   type ResType = InferResponseType<typeof client.api.tasks.$get>
-
-//   const res = await fetcher<ResType>(url, {
-//     cache: 'force-cache',
-//   })
-
-//   return res
-// }
+import { CreateWorkSpaceModal } from '@/features/workspaces/components/crete-workspace-modal'
 
 const Home = () => {
   return (
     <>
       <UserButton />
+      <CreateWorkSpaceModal />
     </>
   )
 }
