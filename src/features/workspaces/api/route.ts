d1b95fec8a851593c16d35c2bfc62633198b05be
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-02-16 20:42:34 +0900

    ✨ ログインユーザーのworkspace一覧取得API実装

--- src/features/workspaces/api/route.ts
+++ src/features/workspaces/api/route.ts
@@ -4,24 +4,34 @@ import { sessionMiddleware } from '@/lib/auth/session-middleware'
 import { eq } from 'drizzle-orm'
 import { Hono } from 'hono'
 
-const app = new Hono().get('/:id', sessionMiddleware, async (c) => {
-  const user = c.get('user')
+const app = new Hono()
+  .get('/', sessionMiddleware, async (c) => {
+    const user = c.get('user')
 
-  const { id } = c.req.param()
+    const workspaceList = await db.query.workspaces.findMany({
+      where: eq(workspaces.userId, user.id),
+    })
 
-  const workspace = await db.query.workspaces.findFirst({
-    where: eq(workspaces.id, id),
+    return c.json(workspaceList, 200)
   })
+  .get('/:id', sessionMiddleware, async (c) => {
+    const user = c.get('user')
 
-  if (!workspace) {
-    return c.json({ error: { message: 'Workspace not found' } }, 404)
-  }
+    const { id } = c.req.param()
 
-  if (user.id !== workspace.userId) {
-    return c.json({ error: { message: 'Forbidden' } }, 403)
-  }
+    const workspace = await db.query.workspaces.findFirst({
+      where: eq(workspaces.id, id),
+    })
 
-  return c.json(workspace, 200)
-})
+    if (!workspace) {
+      return c.json({ error: { message: 'Workspace not found' } }, 404)
+    }
+
+    if (user.id !== workspace.userId) {
+      return c.json({ error: { message: 'Forbidden' } }, 403)
+    }
+
+    return c.json(workspace, 200)
+  })
 
 export default app
--- src/features/workspaces/server/fetcher.ts
+++ src/features/workspaces/server/fetcher.ts
@@ -5,6 +5,24 @@ import { fetcher } from '@/lib/fetcher'
 import { client } from '@/lib/rpc'
 import type { InferResponseType } from 'hono'
 
+export const getWorkspaces = async () => {
+  type ResType = InferResponseType<typeof client.api.workspaces.$get, 200>
+  const url = client.api.workspaces.$url()
+
+  const session = await getSession()
+
+  const res = await fetcher<ResType>(url, {
+    headers: {
+      // biome-ignore lint/style/useNamingConvention: this is a protected api
+      Authorization: session?.user?.id ?? '',
+    },
+    cache: 'force-cache',
+    next: { tags: [getWorkspacesCacheKey] },
+  })
+
+  return res
+}
+
 export const getWorkspace = async (workspaceId: string) => {
   type ResType = InferResponseType<
     (typeof client.api.workspaces)[':id']['$get'],
