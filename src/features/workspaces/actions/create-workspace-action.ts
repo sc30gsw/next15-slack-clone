3cd2cab2e9a0a956b11abc2446b2473094afeec9
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-02-24 18:48:55 +0900

    ✨ memberをadminで追加し、codeを生成する処理を追加

--- src/features/workspaces/actions/create-workspace-action.ts
+++ src/features/workspaces/actions/create-workspace-action.ts
@@ -1,12 +1,31 @@
 'use server'
 
+import { getWorkspacesCacheKey } from '@/constants/cache-keys'
 import { db } from '@/db/db'
-import { workspaces } from '@/db/schema'
+import { members, workspaces } from '@/db/schema'
 import { createWorkspaceInputSchema } from '@/features/workspaces/types/schemas/create-workspace-input-schema'
 import { getSession } from '@/lib/auth/session'
 import { parseWithZod } from '@conform-to/zod'
+import { revalidateTag } from 'next/cache'
+import { redirect } from 'next/navigation'
 
-export const createWorkspaceAction = async (_: unknown, formData: FormData) => {
+const generateCode = () => {
+  const code = Array.from(
+    { length: 6 },
+    () =>
+      '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'[
+        Math.floor(Math.random() * 62)
+      ],
+  ).join('')
+
+  return code
+}
+
+export const createWorkspaceAction = async (
+  _: unknown,
+  formData: FormData,
+  isRedirect: boolean,
+) => {
   const submission = parseWithZod(formData, {
     schema: createWorkspaceInputSchema,
   })
@@ -15,30 +34,37 @@ export const createWorkspaceAction = async (_: unknown, formData: FormData) => {
     return submission.reply()
   }
 
-  try {
-    const session = await getSession()
+  const session = await getSession()
 
-    if (!session?.user?.id) {
-      throw new Error('Unauthorized')
-    }
-
-    const [newWorkspace] = await db
-      .insert(workspaces)
-      .values({
-        name: submission.value.name,
-        // TODO: Create a join code
-        joinCode: '123456',
-        userId: session.user.id,
-      })
-      .returning()
-
-    return {
-      status: 'success',
-      initialValue: {
-        name: newWorkspace.id,
-      },
-    } as const satisfies ReturnType<typeof submission.reply>
-  } catch (_) {
-    throw new Error('Something went wrong')
+  if (!session?.user?.id) {
+    throw new Error('Unauthorized')
   }
+
+  const [newWorkspace] = await db
+    .insert(workspaces)
+    .values({
+      name: submission.value.name,
+      joinCode: generateCode(),
+      userId: session.user.id,
+    })
+    .returning()
+
+  await db.insert(members).values({
+    userId: session.user.id,
+    workspaceId: newWorkspace.id,
+    role: 'admin',
+  })
+
+  revalidateTag(getWorkspacesCacheKey)
+
+  if (isRedirect) {
+    redirect(`/workspace/${newWorkspace.id}`)
+  }
+
+  return {
+    status: 'success',
+    initialValue: {
+      name: newWorkspace.id,
+    },
+  } as const satisfies ReturnType<typeof submission.reply>
 }
