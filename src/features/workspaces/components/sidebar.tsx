b1187e490b8ed59c7cc583ce75234496ffeb067f
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-02-24 18:47:35 +0900

    🏷️ 引数の形を修正

--- src/features/workspaces/components/sidebar.tsx
+++ src/features/workspaces/components/sidebar.tsx
@@ -20,7 +20,7 @@ type SidebarProps = {
 
 export const Sidebar = ({ workspaceId }: SidebarProps) => {
   const workspacesPromise = getWorkspaces()
-  const workspacePromise = getWorkspace(workspaceId)
+  const workspacePromise = getWorkspace({ param: { id: workspaceId } })
 
   return (
     <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-4">
