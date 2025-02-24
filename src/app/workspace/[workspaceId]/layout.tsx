a8def066d43c9506707c0746aaf0af70ab62eba9
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-02-24 18:48:02 +0900

    ➕ react-resizable-panelsを追加

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,17 +1,32 @@
+'use client'
+
 import { CreateWorkSpaceModal } from '@/features/workspaces/components/crete-workspace-modal'
 import type { ReactNode } from 'react'
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels'
 
 const WorkspaceIdLayout = ({
   children,
   toolbar,
   sidebar,
-}: { children: ReactNode; toolbar: ReactNode; sidebar: ReactNode }) => {
+  workspaceSidebar,
+}: {
+  children: ReactNode
+  toolbar: ReactNode
+  sidebar: ReactNode
+  workspaceSidebar: ReactNode
+}) => {
   return (
     <div className="h-full">
       {toolbar}
       <section className="flex h-[calc(100vh-40px)]">
         {sidebar}
-        {children}
+        <PanelGroup direction="horizontal" autoSaveId="ca-workspace-layout">
+          <Panel defaultSize={20} minSize={11} className="bg-[#5E2C5F]">
+            {workspaceSidebar}
+          </Panel>
+          <PanelResizeHandle />
+          <Panel minSize={20}>{children}</Panel>
+        </PanelGroup>
       </section>
       <CreateWorkSpaceModal />
     </div>
