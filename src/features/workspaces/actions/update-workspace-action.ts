31e3d4d9960864e54745a52328afd1304eea0fd2
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-03-01 12:05:23 +0900

    ✨ workspace更新機能の作成

---
+++ src/features/workspaces/actions/update-workspace-action.ts
@@ -0,0 +1,48 @@
+'use server'
+import { getWorkspacesCacheKey } from '@/constants/cache-keys'
+import { db } from '@/db/db'
+import { members, workspaces } from '@/db/schema'
+import { updateWorkspaceInputSchema } from '@/features/workspaces/types/schemas/update-workspace-input-schema'
+import { getSession } from '@/lib/auth/session'
+import { parseWithZod } from '@conform-to/zod'
+import { and, eq } from 'drizzle-orm'
+import { revalidateTag } from 'next/cache'
+import { unauthorized } from 'next/navigation'
+
+export const updateWorkspaceAction = async (_: unknown, formData: FormData) => {
+  const submission = parseWithZod(formData, {
+    schema: updateWorkspaceInputSchema,
+  })
+
+  if (submission.status !== 'success') {
+    return submission.reply()
+  }
+
+  const session = await getSession()
+
+  if (!session?.user?.id) {
+    unauthorized()
+  }
+
+  const member = await db.query.members.findFirst({
+    where: and(
+      eq(members.workspaceId, submission.value.id),
+      eq(members.userId, session.user.id),
+    ),
+  })
+
+  if (!member || member.role !== 'admin') {
+    unauthorized()
+  }
+
+  await db
+    .update(workspaces)
+    .set({
+      name: submission.value.name,
+    })
+    .where(eq(workspaces.id, submission.value.id))
+
+  revalidateTag(`${getWorkspacesCacheKey}/${submission.value.id}`)
+
+  return submission.reply()
+}
