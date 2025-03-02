b9f9acd5bbd39169a5a7aa85b5c6c1656091ee9a
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-03-02 16:47:27 +0900

    ⚡️ SGとなるようmiddlewareに処理追加

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,6 @@
-import { getSession } from '@/lib/auth/session'
-import { redirect } from 'next/navigation'
 import type { ReactNode } from 'react'
 
-const AuthLayout = async ({ children }: { children: ReactNode }) => {
-  const session = await getSession()
-
-  if (session) {
-    redirect('/')
-  }
-
+const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="h-full flex items-center justify-center bg-[#5C3B58]">
       <div className="md:h-auto md:w-[420px]">{children}</div>
--- src/middleware.ts
+++ src/middleware.ts
@@ -8,12 +8,19 @@ export default auth((req) => {
     return NextResponse.redirect(new URL('/sign-in', req.nextUrl))
   }
 
+  if (
+    req.nextUrl.pathname === '/sign-in' ||
+    req.nextUrl.pathname === '/sign-up'
+  ) {
+    return NextResponse.redirect(new URL('/', req.nextUrl))
+  }
+
   return NextResponse.next()
 })
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|sign-in|sign-up|api|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/((?!_next|api|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
   ],
 }
