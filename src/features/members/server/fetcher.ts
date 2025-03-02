0b4ebb6d71d51300856496e17dc05c309c040722
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-03-02 16:50:17 +0900

    ✨ ログインユーザーのmemberデータ取得・workspaceのmember一覧を取得するfetcher作成

--- src/features/members/server/fetcher.ts
+++ src/features/members/server/fetcher.ts
@@ -1,9 +1,38 @@
-import { getWorkspaceMembersCacheKey } from '@/constants/cache-keys'
+import {
+  getWorkspaceCurrentMemberCacheKey,
+  getWorkspaceMembersCacheKey,
+} from '@/constants/cache-keys'
 import { fetcher } from '@/lib/fetcher'
 import { client } from '@/lib/rpc'
 import type { InferRequestType, InferResponseType } from 'hono'
 import 'server-only'
 import { unstable_cacheTag as cacheTag } from 'next/cache'
+
+export const getWorkspaceCurrentMember = async (
+  req: InferRequestType<
+    (typeof client.api.members)[':workspaceId']['current-member']['$get']
+  > &
+    Partial<Record<'userId', string>>,
+) => {
+  'use cache'
+  cacheTag(`${getWorkspaceCurrentMemberCacheKey}/${req.param.workspaceId}`)
+
+  const url = client.api.members[':workspaceId']['current-member'].$url({
+    param: req.param,
+  })
+  type ResType = InferResponseType<
+    (typeof client.api.members)[':workspaceId']['current-member']['$get'],
+    200
+  >
+
+  const res = await fetcher<ResType>(url, {
+    headers: {
+      Authorization: req.userId ?? '',
+    },
+  })
+
+  return res
+}
 
 export const getWorkspaceMembers = async (
   req: InferRequestType<(typeof client.api.members)[':workspaceId']['$get']> &
@@ -12,7 +41,9 @@ export const getWorkspaceMembers = async (
   'use cache'
   cacheTag(`${getWorkspaceMembersCacheKey}/${req.param.workspaceId}`)
 
-  const url = client.api.members[':workspaceId'].$url({ param: req.param })
+  const url = client.api.members[':workspaceId'].$url({
+    param: req.param,
+  })
   type ResType = InferResponseType<
     (typeof client.api.members)[':workspaceId']['$get'],
     200
@@ -24,5 +55,5 @@ export const getWorkspaceMembers = async (
     },
   })
 
-  return res
+  return res.members
 }
