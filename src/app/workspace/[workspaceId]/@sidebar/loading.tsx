840da9dd45e9261f2be90fadd8c68b47f1f8b4f7
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-03-12 17:19:07 +0900

    💄 スタイリング修正

--- src/app/workspace/[workspaceId]/@sidebar/loading.tsx
+++ src/app/workspace/[workspaceId]/@sidebar/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@/components/justd/ui'
 
 const SidebarLoading = () => {
   return (
-    <>
+    <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-4">
       <Skeleton className="size-9 bg-zinc-400/40" />
       <div className="flex flex-col items-center gap-1">
         <Skeleton className="size-9 bg-zinc-400/40" />
@@ -20,7 +20,7 @@ const SidebarLoading = () => {
         <Skeleton className="size-9 bg-zinc-400/40" />
         <Skeleton className="h-2 w-7.5 bg-zinc-400/40" />
       </div>
-    </>
+    </aside>
   )
 }
 
