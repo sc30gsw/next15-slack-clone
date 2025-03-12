8bc36ac29a8e29e4c96cc4d74dd08f9217bea248
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-03-12 14:48:05 +0900

    👔 ページアクセス時のロジックを作成

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,9 +1,24 @@
+import { getChannels } from '@/features/channels/server/fetcher'
+import { WorkspaceIdPageClient } from '@/features/workspaces/components/workspace-id-page-client'
+import { getSession } from '@/lib/auth/session'
+import { redirect } from 'next/navigation'
+
 const WorkspaceIdPage = async ({
   params,
 }: { params: Promise<{ workspaceId: string }> }) => {
   const { workspaceId } = await params
+  const session = await getSession()
 
-  return <div>{workspaceId}</div>
+  const channels = await getChannels({
+    param: { workspaceId },
+    userId: session?.user?.id,
+  })
+
+  if (channels.length > 0) {
+    redirect(`/workspace/${workspaceId}/channel/${channels[0].id}`)
+  }
+
+  return <WorkspaceIdPageClient />
 }
 
 export default WorkspaceIdPage
