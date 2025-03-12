eb7ba8a152c044ba09602c2b8fc45c2eb04223ad
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-03-12 14:49:31 +0900

    💄 sizeの修正

--- src/app/join/[workspaceId]/loading.tsx
+++ src/app/join/[workspaceId]/loading.tsx
@@ -3,7 +3,7 @@ import { Loader } from '@/components/justd/ui'
 const JoinLoading = () => {
   return (
     <div className="h-full flex items-center justify-center">
-      <Loader className="size-8" />
+      <Loader size="large" />
     </div>
   )
 }
