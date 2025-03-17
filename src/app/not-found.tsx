13ac65e699c29e1626be3928fb488510dd0c056d
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-03-17 12:24:16 +0900

    👔 メッセージ取得関数作成

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,12 +6,26 @@ const NotFound = async () => {
   const referer = headersList.get('referer')
 
   const isWorkspacePage = referer?.includes('workspace')
+  const isChannelPage = referer?.includes('channel')
+
+  const getErrorMessage = (referer: 'workspace' | 'channel' | 'default') => {
+    switch (referer) {
+      case 'workspace':
+        return 'No channel found'
+      case 'channel':
+        return 'Channel not found'
+      default:
+        return 'This page is not found'
+    }
+  }
 
   return (
-    <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
+    <div className="h-full flex-1 flex items-center justify-center flex-col gap-y-2">
       <IconTriangleExclamation className="size-6 text-muted-foreground" />
       <span className="text-sm text-fg">
-        {isWorkspacePage ? 'Workspace not found' : 'This page is not found'}
+        {getErrorMessage(
+          isWorkspacePage ? 'workspace' : isChannelPage ? 'channel' : 'default',
+        )}
       </span>
     </div>
   )
