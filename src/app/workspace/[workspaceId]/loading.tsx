6cd7adadf450e52fca7182b2513caf260f0f6780
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-03-24 10:24:33 +0900

    🩹 animate-spin追加

--- src/app/workspace/[workspaceId]/loading.tsx
+++ src/app/workspace/[workspaceId]/loading.tsx
@@ -3,7 +3,7 @@ import { Loader } from '@/components/justd/ui'
 const WorkspaceIdLoading = () => {
   return (
     <div className="h-full flex items-center justify-center flex-col gap-y-2">
-      <Loader size="medium" intent="secondary" />
+      <Loader size="medium" intent="secondary" className="animate-spin" />
     </div>
   )
 }
